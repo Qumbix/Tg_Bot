8bf2780f3a20eb3576f415bbce9b577692d7712e
Qumbix <80636147+Qumbix@users.noreply.github.com>
2021-03-28 18:30:27 +0300

    Update bot_for_command_work.py

--- bot_for_command_work.py
+++ bot_for_command_work.py
@@ -203,7 +203,7 @@ def main():
     #Обязательно установите use_context = True,
     #чтобы использовать новые обратные вызовы на основе контекста
     #Post version 12 this will no longer be necessary
-    updater = Updater("TOKEN", use_context=True)
+    updater = Updater("1689209360:AAFuMleRk7PCL4u_3nfu6lBQq2KbCVCx2Fs", use_context=True)
 
 
     #Создадим обработчик событий для команд
